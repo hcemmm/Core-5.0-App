2b0cec351ec86b494e60123d1275c4fa8fd97fc5
hcemmm <hcemmm@gmail.com>
2023-03-20 17:27:05 +0300

    Bildirim özelliği eklenmeye başlandı

--- Core 5.0 App/ViewComponents/Writer/WriterNotification.cs
+++ Core 5.0 App/ViewComponents/Writer/WriterNotification.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_5._0_App.ViewComponents.Writer
 {
 	public class WriterNotification : ViewComponent
 	{
-		public IViewComponentResult Invoke()
-		{
-			return View();
-		}
-	}
+        NotificationManager _notificationManager = new NotificationManager(new EfNotificationRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _notificationManager.GetList();
+            return View(values);
+        }
+    }
 }
